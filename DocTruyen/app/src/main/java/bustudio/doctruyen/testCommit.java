58ad116a5bfae2cdbc0911781cc3b6dc665b9f9b
TieuHoanToLy96 <tieuhoantoly@gmail.com>
2017-04-08 10:02:50 +0700

    tét

---
+++ DocTruyen/app/src/main/java/bustudio/doctruyen/testCommit.java
@@ -0,0 +1,17 @@
+package bustudio.doctruyen;
+
+/**
+ * Created by TieuHoan on 08/04/2017.
+ */
+
+public class testCommit {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
